8889f3b4499a3ff9a6740c50ea57048a4878e3f9
Alberto <albertopimentel04@gmail.com>
2022-06-22 21:08:22 -0500

    navbar

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -12,11 +12,25 @@ const initialState = {
 
 function ContextProvider({ children }) {
   const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  const handleClick = (clicked) => {
+    setIsClicked({
+      ...initialState,
+      [clicked]: true,
+    });
+  };
   return (
     <StateContext.Provider
       value={{
         activeMenu,
         setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
       }}
     >
       {children}
